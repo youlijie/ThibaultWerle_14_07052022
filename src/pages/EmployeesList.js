d04b6becdaf32074b6558f405e8bd83321cb3f1f
youlijie <63795226+youlijie@users.noreply.github.com>
2022-05-10 19:51:40 +0200

    Search added

--- src/pages/EmployeesList.js
+++ src/pages/EmployeesList.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import { Link } from 'react-router-dom';
 import { EmployeeContext } from '../data/context';
 import DataTable from "react-data-table-component";
@@ -7,15 +7,25 @@ import { columns } from '../data/columns';
 const EmployeesList = () => {
 
     const { employees } = useContext(EmployeeContext)
+    const [employee, setEmployee] = useState(employees);
+
+    const search = (e) => {
+        const newValue = employees.filter(employee => {
+            return employee.firstname.toLowerCase().includes(e.target.value.toLowerCase()) || employee.lastname.toLowerCase().includes(e.target.value.toLowerCase())
+        }
+        )
+        setEmployee(newValue)
+    }
+    
 
     return (
         <div className='Employees-List-Page'>
             <Link to='/' className='btn-link top-btn'> Create a new employee </Link>
             <h1>Current Employees</h1>
-            <DataTable columns = {columns} data = {employees} subHeader pagination selectableRows/>  
+            <DataTable columns = {columns} data = {employee} subHeader pagination selectableRows/>  
             <div className = "research-contenair">
                 <label htmlFor="research">Search: </label>
-                <input type="text" id="research" />
+                <input type="text" id="research" onChange={search} />
             </div>  
         </div>
     );
